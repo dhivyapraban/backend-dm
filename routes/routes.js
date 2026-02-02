b5a2bf0d800a18471af28cacca5b9b61ccf7b3b1
PremnathSundarrajan <premsundarrajan4@gmail.com>
2026-02-03 01:09:16 +0530

    create and assign deliver api

--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,9 @@ const { authenticateToken, requireRole } = require('../middleware/auth');
 
 router.use(authenticateToken);
 
+// Advanced route creation with driver relay logic
+router.post('/create-and-assign', requireRole('DISPATCHER'), routeController.createAndAssignRoute);
+
 // Triggered by Dispatcher
 router.post('/allocate', requireRole('DISPATCHER'), routeController.allocateRoutes);
 
